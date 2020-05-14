c0d06636efe11f944b693a292e66d1ab68906cad
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-13 22:38:47 -0400

    added last card info to portfolio page

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import { MDBCard, MDBCardTitle, MDBBtn, MDBCardGroup, MDBCardImage, MDBCardText, MDBCardBody } from "mdbreact";
 
 
@@ -13,11 +13,11 @@ const PortfolioComponent = () => {
         <MDBCardBody>
           <MDBCardTitle tag="h5">Achoo App</MDBCardTitle>
           <MDBCardText>
-          Achoo focuses on the general public’s inability to distinguish between
-                    the common cold and flu, and this mishap can lead to severe consequences.
-                    Users are asked a series of questions to determine whether or not they have the cold or flu.
-                    Often times people mistake the flu for the common cold, and this is what has led to many
-                    deaths. Being able to recognize and differentiate between the cold and flu, will save many lives.
+            Achoo focuses on the general public’s inability to distinguish between
+            the common cold and flu, and this mishap can lead to severe consequences.
+            Users are asked a series of questions to determine whether or not they have the cold or flu.
+            Often times people mistake the flu for the common cold, and this is what has led to many
+            deaths. Being able to recognize and differentiate between the cold and flu, will save many lives.
                     {/* <p href="https://ksuares1.github.io/Project1/"></p> */}
           </MDBCardText>
           <MDBBtn href="https://ksuares1.github.io/Project1/" color="primary" size="md">
@@ -32,8 +32,8 @@ const PortfolioComponent = () => {
         <MDBCardBody>
           <MDBCardTitle tag="h5">Anticipate App</MDBCardTitle>
           <MDBCardText>
-          Anticipate is a voice recording app that connects nurses and family members with patients.
-           Nurses are able to sign-up and login to the patient database with back-end languages such as Node.js, Mongodb, and Mongoose. Patients' family members are able to view their family members medications through our database portal.
+            Anticipate is a voice recording app that connects nurses and family members with patients.
+            Nurses are able to sign-up and login to the patient database with back-end languages such as Node.js, Mongodb, and Mongoose. Patients' family members are able to view their family members medications through our database portal.
            <p></p>
           </MDBCardText>
           <MDBBtn href="https://github.com/ksuares1/anticipate" color="primary" size="md">
@@ -46,13 +46,13 @@ const PortfolioComponent = () => {
         <MDBCardImage src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg" alt="MDBCard image cap" top hover
           overlay="white-slight" />
         <MDBCardBody>
-          <MDBCardTitle tag="h5">Panel title</MDBCardTitle>
+          <MDBCardTitle tag="h5">Liri Node App</MDBCardTitle>
           <MDBCardText>
-            Some quick example text to build on the card title and make up
-            the bulk of the card's content.
+            LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives you back data.
+            This application searches Spotify for songs, Bands in Town for concerts, and OMDB for movies.
           </MDBCardText>
-          <MDBBtn color="primary" size="md">
-            read more
+          <MDBBtn href="https://github.com/ksuares1/liri-node-app" color="primary" size="md">
+            Learn More
           </MDBBtn>
         </MDBCardBody>
       </MDBCard>
