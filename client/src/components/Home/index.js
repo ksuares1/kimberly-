f976ece163cdd2dbe7247c1ec8d01f8779e7035f
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-18 19:06:09 -0400

    added background cover sizing

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-// import './index.css';
+import './index.css';
 import main from '../../assets/img/port-main.jpg';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
 
 const HomeComponent = () => {
     return (
-       
-        <Jumbotron  style={{backgroundImage:`url(${main})`}}>
-        <h1>Hi! I'm Kimberly Suares a Full Stack Web Developer.</h1>
-        <p>
-          .
+
+        <Jumbotron className="container" style={{ backgroundImage: `url(${main})` }}>
+            <h1>Hi! I'm Kimberly Suares a Full Stack Web Developer.</h1>
+            <p>
+                
         </p>
-        <p>
-          <Button variant="primary">Find out more</Button>
-        </p>
-      </Jumbotron>
-      
-            
-    
+            <p>
+                <Button href="/about" variant="primary">Find out more</Button>
+            </p>
+        </Jumbotron>
+
+
+
 
 
     );
