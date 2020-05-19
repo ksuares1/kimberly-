c43bd0796b2bb6d378357062d180170d92b37c40
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-19 13:26:30 -0400

    added more text to about

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -17,15 +17,17 @@ function AboutComponent() {
             <br>
             </br>
             <Row>
-            <Col xs={6} md={4}>
-                <img className="about-img" src={require("../../assets/img/profile.JPG")} alt="Kimberly Suares" />
-            <p className="about-text" style={{ display: "inline-block", width:'50%', height:'50%'}}>
-                Hi, I'm Kimberly Suares! 
-                I'm a Full Stack Web Developer that specializes in health focused applications.
-                My coding journey started almost six years ago, after I took an online business course.
-                I quickly fell in love with creating something from scratch, after completing my first homework assignment to create a Wordpress website. 
-                During my years of learning to code, I volunteered at a local hospital called New York Presbyterian Brooklyn Methodist, where I visited patients, asked them questions, 
-                and provided them with information about their care once they left the hospital. This hospital used very little technology to interact with its patients, and that's when I realized how technology could effectively change patients lives! </p>
+                <Col xs={6} md={4}>
+                    <img className="about-img" src={require("../../assets/img/profile.JPG")} alt="Kimberly Suares" />
+                    <p className="about-text" style={{ display: "inline-block", width: '50%', height: '50%' }}>
+                        Hi, I'm Kimberly Suares!
+                        I'm a Full Stack Web Developer that specializes in health focused applications.
+                        My coding journey started almost six years ago, after I took an online business course.
+                        I quickly fell in love with creating something from scratch, after completing a Wordpress website as my first homework assignment.
+                        During my years of learning to code, I volunteered at a local hospital called New York Presbyterian Brooklyn Methodist, where I visited patients; asked them questions,
+                and provided them with information about their care once they left the hospital. This hospital used very little technology to interact with its patients, and that's when I realized how technology could effectively change patients lives!
+                Today, I build applications to help users create healthier lifestyles, establish a deeper understanding of their medical health, and connection with their doctor's. 
+                </p>
 
                 </Col>
             </Row>
