10455e4715698818f4d349f4b4269800617e7d22
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-18 23:15:25 -0400

    added a row and col to about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import './index.css';
-// import Col from "react-bootstrap/Col";
-// import Row from "react-bootstrap/Row";
-// import Container from "react-bootstrap/Container";
-// import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+
 
 function AboutComponent() {
     return (
-        // <Container>
-        //     <Row>
-        //         <Col xs={6} md={4}>
-        //             <Image className="about-img" src={require("../../assets/img/profile.JPG")} alt="Kimberly Suares" rounded />
-        //         </Col>
-
-        //     </Row>
-        // </Container>
         <div>
             <br>
             </br>
@@ -24,7 +16,12 @@ function AboutComponent() {
             </br>
             <br>
             </br>
-            <img className="about-img" src={require("../../assets/img/profile.JPG")} alt="Kimberly Suares"/>
+            <Row>
+            <Col xs={6} md={4}>
+                <img className="about-img" src={require("../../assets/img/profile.JPG")} alt="Kimberly Suares" />
+                <p></p>
+                </Col>
+            </Row>
         </div>
 
 
