c7502a009b93e39c8fe94f7dc1fadf8b408eec10
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-18 23:59:06 -0400

    added more text to about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -21,7 +21,12 @@ function AboutComponent() {
                 <img className="about-img" src={require("../../assets/img/profile.JPG")} alt="Kimberly Suares" />
             <p className="about-text" style={{ display: "inline-block", width:'50%', height:'50%'}}>
                 Hi, I'm Kimberly Suares! 
-                I am a Full Stack Web Developer that specializes in health focused applications.</p>
+                I am a Full Stack Web Developer that specializes in health focused applications.
+                My coding journey started almost six years ago, after I took an online business course.
+                The first assignment of this course was to create a Wordpress website. I quickly fell in love with creating something from scratch.
+                During my years of learning to code, I volunteered at a local hospital called New York Presbyterian Brooklyn Methodist, where I visited patients, asked them questions, 
+                and provided them with information about their care once they left the hospital. </p>
+
                 </Col>
             </Row>
         </div>
