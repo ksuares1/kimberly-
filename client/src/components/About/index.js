639d5ee7a92dd72cd9b4c3f1f0d428036495b072
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-19 13:37:41 -0400

    finished added text to about

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -27,7 +27,7 @@ function AboutComponent() {
                         During my years of learning to code, I volunteered at a local hospital called New York Presbyterian Brooklyn Methodist, where I visited patients; asked them questions,
                 and provided them with information about their care once they left the hospital. This hospital used very little technology to interact with its patients, and that's when I realized how technology could effectively change patients lives!
                 Today, I build applications to help users create healthier lifestyles, establish a deeper understanding of their medical health, and connection with their doctor's. 
-                </p>
+                As a graduate of Columbia University's Coding Engineering Bootcamp, I have developed invaluable skills and knowledge in a variety of coding languages such as: HTML5, CSS, JavaScript, JQuery, Bootstrap, Mongodb, Express, Node.js, Sequelize, AJAX/Axios, and more. </p>
 
                 </Col>
             </Row>
