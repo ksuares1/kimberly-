093984c1b67c4fe9f9a31be0867c2613f8939415
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-19 16:13:35 -0400

    added textarea to contact page

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -3,29 +3,40 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 
-function ContactForm(){
-    return(
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
+function ContactForm() {
+  return (
+    <Form>
+      <br>
+      </br>
+      <h1>Contact Me</h1>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="name" placeholder="Enter Name" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
     </Form.Text>
-  </Form.Group>
+      </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
+      <div className="form-group">
+        <label htmlFor="exampleFormControlTextarea1">
+          Message
+            </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="5"
+        />
+      </div>
+      <Button variant="primary" type="submit">
+        Submit
   </Button>
-</Form>
+    </Form>
 
-    )
+  )
 }
 export default ContactForm;
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,7 +3,13 @@ import ContactForm from "../components/Contact";
 
 const ContactPage=()=>{
     return(
+        <div>
+            <br>
+            </br>
+            <br>
+            </br>
         <ContactForm/>
+        </div>
     )
 }
 export default ContactPage;
