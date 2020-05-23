274a7d60f43d87e3e033450d41eb5d6b32aa4360
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-22 22:54:41 -0400

    added more icons

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -7,6 +7,8 @@ function SkillCompnent() {
             <h1>Technologies</h1>
             <br>
             </br>
+            <br>
+            </br>
             <div className="skills-conatainer">
                 {/* style="margin: 0 auto;" */}
                 <div className="featured-on-alerts">
@@ -16,6 +18,11 @@ function SkillCompnent() {
                     <img src={require("../../assets/img/expressjs-icon.jpg")} alt="express-icon" width="120" height="120" />
                     <img src={require("../../assets/img/javascript-icon.png")} alt="JavaScript-logo" width="75" height="75" /> 
                     <img src={require("../../assets/img/jquery-icon.png")} alt="Jquery-icon" width="75" height="75" /> 
+                    <img src={require("../../assets/img/firebase-icon.png")} alt="firebase-icon" width="75" height="75" /> 
+                    <img src={require("../../assets/img/mysql-icon.png")} alt="Mysql-icon" width="75" height="75" /> 
+                    <img src={require("../../assets/img/mongodb-icon.png")} alt="Jquery-icon" width="85" height="85" /> 
+                    <img src={require("../../assets/img/bootstrap-icon.png")} alt="Bootstrap-icon" width="75" height="75" /> 
+                    <img src={require("../../assets/img/react.png")} alt="React-icon" width="75" height="75" /> 
 
                    
                 </div>
