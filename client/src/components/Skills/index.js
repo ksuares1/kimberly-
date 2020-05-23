64aa62dad72e36ee0900a715b69c48e73d303fa7
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-22 22:37:25 -0400

    Added more icons to skills

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -14,7 +14,9 @@ function SkillCompnent() {
                     <img src={require("../../assets/img/html-icon.png")} alt="HTML5-icon" width="85" height="85" />
                     <img src={require("../../assets/img/node-icon.png")} alt="Node-icon" width="100" height="100" />
                     <img src={require("../../assets/img/expressjs-icon.jpg")} alt="express-icon" width="120" height="120" />
-                {/* <img src={require("../../assets/img/")} alt="nyt-logo" width="100" height="100" />  */}
+                    <img src={require("../../assets/img/javascript-icon.png")} alt="JavaScript-logo" width="75" height="75" /> 
+                    <img src={require("../../assets/img/jquery-icon.png")} alt="Jquery-icon" width="75" height="75" /> 
+
                    
                 </div>
             </div>
