23f1bddd63276e0dad2f3bbf5ae8a435eead63e1
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-22 22:31:36 -0400

    updated skills icons

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -3,22 +3,22 @@ import React from "react";
 
 function SkillCompnent() {
     return (
-    <main>
+        <main>
             <h1>Skills</h1>
             <br>
             </br>
             <div className="skills-conatainer">
-             {/* style="margin: 0 auto;" */}
+                {/* style="margin: 0 auto;" */}
                 <div className="featured-on-alerts">
-                    {/* <div className="text-center alert alert-success bg-transparent" role="alert"> */}
-                         <img src={require("../../assets/img/html-icon.png")} alt="HTML5-icon" width="65" height="65" />
-                 <img src={require("../../assets/img/node-icon.png")} alt="Node-icon" width="100" height="100" />
-                {/* <img src="img/abc-7-logo.png" alt="nyt-logo" width="100" height=100 />
-                <img src="img/the-view-logo.png" alt="nyt-logo" width="100" height=100 /> */}
-                    {/* </div> */}
-                </div> 
+                   
+                    <img src={require("../../assets/img/html-icon.png")} alt="HTML5-icon" width="85" height="85" />
+                    <img src={require("../../assets/img/node-icon.png")} alt="Node-icon" width="100" height="100" />
+                    <img src={require("../../assets/img/expressjs-icon.jpg")} alt="express-icon" width="120" height="120" />
+                {/* <img src={require("../../assets/img/")} alt="nyt-logo" width="100" height="100" />  */}
+                   
                 </div>
-                </main>
+            </div>
+        </main>
     )
 }
 
