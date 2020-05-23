ee7c28bb75853aa8939211d3b5240b9670ed9821
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-23 14:47:51 -0400

    updating about page

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -10,23 +10,22 @@ function SkillCompnent() {
             <br>
             </br>
             <div className="skills-conatainer">
-                {/* style="margin: 0 auto;" */}
                 <div className="featured-on-alerts">
-                   
+
                     <img src={require("../../assets/img/html-icon.png")} alt="HTML5-icon" width="85" height="85" />
                     <img src={require("../../assets/img/css-icon.png")} alt="Css-icon" width="100" height="100" />
                     <img src={require("../../assets/img/node-icon.png")} alt="Node-icon" width="100" height="100" />
                     <img src={require("../../assets/img/expressjs-icon.jpg")} alt="express-icon" width="120" height="120" />
-                    <img src={require("../../assets/img/javascript-icon.png")} alt="JavaScript-logo" width="75" height="75" /> 
-                    <img src={require("../../assets/img/jquery-icon.png")} alt="Jquery-icon" width="75" height="75" /> 
-                    <img src={require("../../assets/img/firebase-icon.png")} alt="firebase-icon" width="75" height="75" /> 
-                    <img src={require("../../assets/img/mysql-icon.png")} alt="Mysql-icon" width="75" height="75" /> 
-                    <img src={require("../../assets/img/mongodb-icon.png")} alt="Jquery-icon" width="85" height="85" /> 
-                    <img src={require("../../assets/img/bootstrap-icon.png")} alt="Bootstrap-icon" width="75" height="75" /> 
-                    <img src={require("../../assets/img/react.png")} alt="React-icon" width="75" height="75" /> 
-                    <img src={require("../../assets/img/ajax.jpg")} alt="Ajax-icon" width="75" height="75" /> 
+                    <img src={require("../../assets/img/javascript-icon.png")} alt="JavaScript-logo" width="75" height="75" />
+                    <img src={require("../../assets/img/jquery-icon.png")} alt="Jquery-icon" width="75" height="75" />
+                    <img src={require("../../assets/img/firebase-icon.png")} alt="firebase-icon" width="75" height="75" />
+                    <img src={require("../../assets/img/mysql-icon.png")} alt="Mysql-icon" width="75" height="75" />
+                    <img src={require("../../assets/img/mongodb-icon.png")} alt="Jquery-icon" width="85" height="85" />
+                    <img src={require("../../assets/img/bootstrap-icon.png")} alt="Bootstrap-icon" width="75" height="75" />
+                    <img src={require("../../assets/img/react.png")} alt="React-icon" width="75" height="75" />
+                    <img src={require("../../assets/img/ajax.jpg")} alt="Ajax-icon" width="75" height="75" />
 
-                   
+
                 </div>
             </div>
         </main>
