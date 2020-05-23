b29470ce510d5802a096de4c9a7b61ca6aa51c58
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-23 14:55:26 -0400

    added skills to about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -7,7 +7,7 @@ import Row from "react-bootstrap/Row";
 
 function AboutComponent() {
     return (
-        <div>
+        <main>
             <br>
             </br>
             <br>
@@ -34,28 +34,38 @@ function AboutComponent() {
                     </div>
                 </Col>
             </Row>
-        </div>
+            <br>
+            </br>
+            <br>
+            </br>
+            <br>
+            </br>
 
-        // <>
-        //     <div className="skills-conatainer">
-        //         <div className="featured-on-alerts">
+            <div>
+                <h1 className="text-align:center">Technologies</h1>
 
-        //             <img src={require("../../assets/img/html-icon.png")} alt="HTML5-icon" width="85" height="85" />
-        //             <img src={require("../../assets/img/css-icon.png")} alt="Css-icon" width="100" height="100" />
-        //             <img src={require("../../assets/img/node-icon.png")} alt="Node-icon" width="100" height="100" />
-        //             <img src={require("../../assets/img/expressjs-icon.jpg")} alt="express-icon" width="120" height="120" />
-        //             <img src={require("../../assets/img/javascript-icon.png")} alt="JavaScript-logo" width="75" height="75" />
-        //             <img src={require("../../assets/img/jquery-icon.png")} alt="Jquery-icon" width="75" height="75" />
-        //             <img src={require("../../assets/img/firebase-icon.png")} alt="firebase-icon" width="75" height="75" />
-        //             <img src={require("../../assets/img/mysql-icon.png")} alt="Mysql-icon" width="75" height="75" />
-        //             <img src={require("../../assets/img/mongodb-icon.png")} alt="Jquery-icon" width="85" height="85" />
-        //             <img src={require("../../assets/img/bootstrap-icon.png")} alt="Bootstrap-icon" width="75" height="75" />
-        //             <img src={require("../../assets/img/react.png")} alt="React-icon" width="75" height="75" />
-        //             <img src={require("../../assets/img/ajax.jpg")} alt="Ajax-icon" width="75" height="75" />
-        //         </div>
-        //     </div>
-        // </>
-       
+                <div className="skills-conatainer">
+                    <div className="featured-on-alerts">
+
+                        <img src={require("../../assets/img/html-icon.png")} alt="HTML5-icon" width="85" height="85" />
+                        <img src={require("../../assets/img/css-icon.png")} alt="Css-icon" width="100" height="100" />
+                        <img src={require("../../assets/img/node-icon.png")} alt="Node-icon" width="100" height="100" />
+                        <img src={require("../../assets/img/expressjs-icon.jpg")} alt="express-icon" width="120" height="120" />
+                        <img src={require("../../assets/img/javascript-icon.png")} alt="JavaScript-logo" width="75" height="75" />
+                        <img src={require("../../assets/img/jquery-icon.png")} alt="Jquery-icon" width="75" height="75" />
+                        <img src={require("../../assets/img/firebase-icon.png")} alt="firebase-icon" width="75" height="75" />
+                        <img src={require("../../assets/img/mysql-icon.png")} alt="Mysql-icon" width="75" height="75" />
+                        <img src={require("../../assets/img/mongodb-icon.png")} alt="Jquery-icon" width="85" height="85" />
+                        <img src={require("../../assets/img/bootstrap-icon.png")} alt="Bootstrap-icon" width="75" height="75" />
+                        <img src={require("../../assets/img/react.png")} alt="React-icon" width="75" height="75" />
+                        <img src={require("../../assets/img/ajax.jpg")} alt="Ajax-icon" width="75" height="75" />
+                    </div>
+                </div>
+            </div>
+        </main>
+
+
+
 
 
     )
