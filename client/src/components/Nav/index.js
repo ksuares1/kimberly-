2f6d48674286cf8154a9129f039a86fe2ca4214d
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-05-27 23:57:59 -0400

    added icons

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-// import Form from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 // import FormControl from "react-bootstrap/FormControl";
 // import Button from "react-bootstrap/Button";
 
@@ -24,10 +24,11 @@ function NavBar() {
               {/* <NavDropdown.Item href="/readygo">ReadyGo</NavDropdown.Item> */}
             </NavDropdown>
           </Nav>
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>  */}
-          {/* </Form> */}
+           <Form inline>
+          <i className="fab fa-github fa-xs"></i>
+          <i className="fab fa-twitter-square fa-xs"></i>
+          <i className="fab fa-linkedin fa-xs"></i> 
+           </Form> 
         </Navbar.Collapse>
       </Navbar>
 
